0a54a4a40dc9995938bb51586787be6d9c22357b
jungo <jungo.rnti@gmail.com>
2013-07-04 16:50:17 +0400

    Fabfile fixes
    - refactored css/js file paths
    - few bugfixes

--- project_template/fabfile.py
+++ project_template/fabfile.py
@@ -4,26 +4,35 @@ import os
 import base64
 
 from fabric.operations import local
-from fabric.context_managers import lcd, settings, hide
-from fabric.api import env, task
+from fabric.context_managers import lcd
 
 
-def _relative_dir(dirs=None):
-    if dirs is None:
-        dirs = []
+def _absolute_dir(*dirs):
+    """Returns absolute path to this file, joined with dirs."""
     return os.path.join(os.path.abspath(os.path.dirname(__file__)), *dirs)
 
 
+CSS_SRC_DIR = _absolute_dir('{{ project_name }}', 'static/css/src')
+CSS_BUILD_DIR = _absolute_dir('{{ project_name }}', 'static/css/build')
+CSS_STYLUS_DIR = _absolute_dir('{{ project_name }}', 'static/css/styl')
+
+JS_SRC_DIR = _absolute_dir('{{ project_name }}', 'static/js/src')
+JS_BUILD_DIR = _absolute_dir('{{ project_name }}', 'static/js/build')
+
+
 def make_virtualenv():
+    """Creates virtualenv in local directory."""
     local('virtualenv env')
 
 
 def install_requirements():
+    """Installs requirements to the local virtualenv."""
     local('source env/bin/activate && pip install -r requirements.txt',
           shell='bash')
 
 
 def generate_secret():
+    """Generates 512-length secret key and writes it to the file `.secret`."""
     SECRET_FILE = os.path.join('{{project_name}}', '.secret')
 
     with open(SECRET_FILE, 'w') as f:
@@ -40,62 +49,92 @@ def on_complete():
 
 
 def install_nodejs(version='0.10.12', cpus=1):
+    """
+    Installs nodejs in local directory.
+
+    Downloads nodejs sources, compiles it in temp directory with `cpus` number
+    of jobs (1 is by default), then installs in local directory. Temporary
+    directory removed after installing.
+    """
     node_file = 'node-v{}.tar.gz'.format(version)
     node_url = 'http://nodejs.org/dist/v{}/{}'.format(version, node_file)
-    gpg_file = 'SHASUMS256.txt'
-    gpg_url = 'http://nodejs.org/dist/v{}/{}'.format(version, gpg_file)
     build_dir = 'fabric_build'
 
-    print 'installing node.js v{}'.format(version)
     local('mkdir -p {}'.format(build_dir))
     with lcd(build_dir):
         local('wget {} -O {}'.format(node_url, node_file))
         local('tar xzf {}'.format(node_file))
         with lcd('node-v{}'.format(version)):
-            local('./configure --prefix={}'.format(_relative_dir()))
+            local('./configure --prefix={}'.format(_absolute_dir()))
             local('make -j{}'.format(cpus))
             local('make install')
     local('rm -rf {}'.format(build_dir))
-    print 'install complete'
 
 
 def install_nodejs_modules():
+    """Installs nodejs minification modules."""
     modules = ['uglify-js', 'stylus', 'clean-css']
     local('bin/npm install -g {}'.format(' '.join(modules)))
 
 
 def minifyjs():
-    src_dir = 'static/js/src'
-    build_dir = 'static/js/build'
-    local('mkdir -p {}'.format(build_dir))
-    js_files = []
-    for root, dirs, files in os.walk(src_dir):
+    """Minifies all *.js files found in js/build directory to js/src/script.js."""
+    with lcd('blog'):
+        local('mkdir -p {}'.format(JS_BUILD_DIR))
+        js_files = []
+        for root, dirs, files in os.walk(JS_SRC_DIR):
+            for fname in files:
+                if fname.endswith('.js'):
+                    js_files.append(os.path.join(root, fname))
+    if js_files:
+        local('bin/uglifyjs {} -o {}/script.min.js'.format(
+            ' '.join(js_files), JS_BUILD_DIR
+        ))
+    else:
+        print 'no javascript files to minify found'
+
+
+def stylus_convert():
+    """Processes all *.styl files from css/styl to css/src."""
+    for root, dirs, files in os.walk(CSS_STYLUS_DIR):
         for fname in files:
-            if fname.endswith('.js'):
-                js_files.append(os.path.join(root, fname))
-    local('bin/uglifyjs {} -o {}/script.js'.format(
-            ' '.join(js_files), build_dir
-          ))
+            if fname.endswith('.styl'):
+                local('bin/stylus < {} > {}'.format(
+                    os.path.join(root, fname),
+                    os.path.join(CSS_SRC_DIR, fname.replace('.styl', '.css')),
+                ))
 
 
 def minifycss():
-    src_dir = 'static/css/src'
-    styl_dir = 'static/css/styl'
-    build_dir = 'static/css/build'
-
-    local('bin/stylus < {}/style.styl > {}/style.css'.format(styl_dir, src_dir))
-    local('cat {src}/normalize.css {src}/foundation.min.css {src}/style.css |'
-          ' bin/cleancss -o {build}/style.css'.format(
-            src=src_dir, build=build_dir
-          ))
+    """Minifies css files from css/src to css/build."""
+    css_files = []
+    for root, dirs, files in os.walk(CSS_SRC_DIR):
+        for fname in files:
+            if fname.endswith('.css'):
+                css_files.append(os.path.join(root, fname))
+    if css_files:
+        local('cat {} | bin/cleancss -o {}/style.min.css'.format(
+            ' '.join(css_files), CSS_BUILD_DIR
+        ))
+    else:
+        print 'no css files to minify found'
 
 
 def minify():
+    """Minifies js and css for whole project."""
     minifyjs()
+    stylus_convert()
     minifycss()
 
 
+def create_user_config_file():
+    # read username
+    # copy configs for username
+    pass
+
+
 def bootstrap():
+    """Installs everything."""
     make_virtualenv()
     install_requirements()
     generate_secret()
