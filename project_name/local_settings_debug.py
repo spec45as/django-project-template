1a5915545662312771f6f20deb166a304bd50cdb
Stanislav Lobanov <n10101010@gmail.com>
2013-07-06 17:45:07 +0400

    Added debug context processor to dev local settings.

--- project_name/local_settings_debug.py
+++ project_name/local_settings_debug.py
@@ -30,3 +30,6 @@ CACHES = {
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 INTERNAL_IPS = ['127.0.0.1']
+
+TEMPLATE_CONTEXT_PROCESSORS += ("django.core.context_processors.debug",)
+
