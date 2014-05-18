e666c360dd3d07701de2267bb897cd047bf39aae
Stanislav Lobanov <n10101010@gmail.com>
2014-05-18 16:47:18 +0400

    Serve media and static under development

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 
 from django.contrib import admin
 admin.autodiscover()
@@ -8,3 +10,9 @@ urlpatterns = patterns('',
     url(r'^$', '{{ project_name }}.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += (
+        static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) +
+        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    )
