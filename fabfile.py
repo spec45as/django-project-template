2a37e3ebbb0c219e1c5250cff42da89be2466bdd
asyncee <n10101010@gmail.com>
2014-09-09 20:15:36 +0400

    Updated fabfile

--- fabfile.py
+++ fabfile.py
@@ -8,12 +8,15 @@ from fabric.operations import local, prompt
 from fabric.context_managers import lcd
 from fabric.contrib.console import confirm
 
+import jinja2
+
 
 # full path to this file
 BASE_PATH = os.path.abspath(os.path.dirname(__file__))
 
 PROJECT_NAME = '{{ project_name }}'
 PROJECT_PATH = os.path.join(BASE_PATH, PROJECT_NAME)
+USER_CONFIG_FILE = 'conf/config.ini'
 
 
 def make_virtualenv():
@@ -21,31 +24,18 @@ def make_virtualenv():
     local('virtualenv env')
 
 
-def install_requirements(wheels=None, noindex=False):
+def install_requirements():
     """
     Installs requirements to the local virtualenv.
     If wheels directory is not None then wheel installation is performed.
     """
-    if wheels is not None:
-        source = 'source env/bin/activate'
-        index = '--no-index' if noindex else ''
-        pip_wheels = ('pip install --use-wheel {no_index} --find-links={wheels}'
-                      ' -r requirements.txt'.format(no_index=index, wheels=wheels))
-        local('{} && {}'.format(source, pip_wheels), shell='bash')
-    else:
-        local('source env/bin/activate && pip install -r requirements.txt',
-              shell='bash')
-
-
-def make_wheels(path='./wheels'):
-    """Packages all requirements into specified directory using wheel format (*.whl)."""
-    local('mkdir -p {}'.format(path))
-    local('pip wheel --wheel-dir={} -r requirements.txt'.format(path))
+    local('source env/bin/activate && pip install -r requirements.txt',
+          shell='bash')
 
 
 def generate_secret():
     """Generates 512-length secret key and writes it to the file `.secret`."""
-    SECRET_FILE = os.path.join(PROJECT_NAME, '.secret')
+    SECRET_FILE = os.path.normpath(os.path.join(PROJECT_NAME, '../conf/secret'))
 
     with open(SECRET_FILE, 'w') as f:
         secret_key = base64.urlsafe_b64encode(os.urandom(512))
@@ -60,30 +50,35 @@ def create_user_config_file():
                           default=getpass.getuser(),
                           validate=r'^.*$')
         with lcd(PROJECT_NAME):
-            ini_file = '.config-dev-{}.ini'.format(username)
-            py_file = 'local_settings_{}.py'.format(username)
-            local('cp .config-dev-example.ini {}'.format(ini_file))
-            local('cp local_settings_debug.py {}'.format(py_file))
-            with open(os.path.join(PROJECT_PATH, py_file), 'r+') as f:
-                data = f.read().replace('.config-dev-example.ini', ini_file)
-                f.seek(0)
-                f.write(data)
-                f.truncate()
+            local_template_path = 'conf/local_settings.template'
+            result_filename = 'local_%s.py' % username
+            result_file_path = os.path.join(PROJECT_PATH, 'settings',
+                                            result_filename)
 
-            print 'created {}'.format(ini_file)
-            print 'created {}'.format(py_file)
+            # create local_<user>.py
+            with open(result_file_path, 'w') as dest, open(local_template_path, 'r') as src:
+                dest.write(src.read())
+                print 'created {}'.format(result_file_path)
+
+            # create config.ini
+            with open('conf/config.ini', 'w') as dest, open('conf/config.ini.template', 'r') as src:
+                dest.write(src.read())
+                print 'created {}'.format(USER_CONFIG_FILE)
+
+            # update manage.py
+            manage_py_template = jinja2.Template(open('conf/manage.py.template').read())
+            output = manage_py_template.render(config_name=result_filename[:-3])
+            with open('manage.py', 'w') as f:
+                f.write(output)
+                print 'updated manage.py'
+
             print ('now you should modify your database settings in {}'
-                   .format(ini_file))
-            print 'you can run django development server with following command:'
-            print ('\tpython manage.py runserver --settings={}'
-                   '.local_settings_{}'.format(PROJECT_NAME, username))
-            print "but remeber to source your virtual env first:"
-            print '\tsource env/bin/activate'
+                   .format(result_file_path))
 
 
-def bootstrap(wheels=None, noindex=False):
+def bootstrap():
     """Installs everything."""
     make_virtualenv()
-    install_requirements(wheels, noindex)
+    install_requirements()
     generate_secret()
     create_user_config_file()
