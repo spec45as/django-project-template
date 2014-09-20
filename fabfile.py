b8acea3a1f083bb8233ff99ff4b655a753ffb137
Stanislav Lobanov <n10101010@gmail.com>
2014-09-20 12:39:44 +0400

    Fabfile refactoring

--- fabfile.py
+++ fabfile.py
@@ -1,83 +1,103 @@
 # coding: utf-8
 
+from __future__ import print_function
+
 import os
 import base64
 import getpass
 
-from fabric.operations import local, prompt
-from fabric.context_managers import lcd
-from fabric.contrib.console import confirm
-
 import jinja2
 
+from fabric.operations import local, prompt
+from fabric.contrib.console import confirm
 
-# full path to this file
+
 BASE_PATH = os.path.abspath(os.path.dirname(__file__))
 
 PROJECT_NAME = '{{ project_name }}'
 PROJECT_PATH = os.path.join(BASE_PATH, PROJECT_NAME)
+SECRET_FILE = 'conf/secret'
 USER_CONFIG_FILE = 'conf/config.ini'
 
 
+def _log(message):
+    print(message)
+
+
+def _render(src, dst, **kwargs):
+    """
+    Отрендерить jinja2 шаблон.
+
+    Аргументы:
+        src - путь к шаблону
+        dst - путь к выходному файлу
+        **kwargs - параметры для передачи в шаблон
+    """
+    with open(src) as f:
+        template = jinja2.Template(f.read())
+    with open(dst, 'w') as f:
+        f.write(template.render(**kwargs))
+
+
+def _rel(*path):
+    """Возвратить полный путь относительно директории проекта."""
+    return os.path.join(BASE_PATH, *path)
+
+
 def make_virtualenv():
-    """Creates virtualenv in local directory."""
+    """Создать виртуальное окружение."""
     local('virtualenv env')
 
 
 def install_requirements():
-    """
-    Installs requirements to the local virtualenv.
-    If wheels directory is not None then wheel installation is performed.
-    """
+    """Установить зависимости в виртуальное окружение."""
     local('source env/bin/activate && pip install -r requirements.txt',
           shell='bash')
 
 
-def generate_secret():
-    """Generates 512-length secret key and writes it to the file `.secret`."""
-    SECRET_FILE = os.path.normpath(os.path.join(PROJECT_NAME, '../conf/secret'))
+def generate_secret(length=512):
+    """Сгенерировать секрет и записать в файл conf/secret."""
 
-    with open(SECRET_FILE, 'w') as f:
-        secret_key = base64.urlsafe_b64encode(os.urandom(512))
+    with open(_rel(SECRET_FILE), 'w') as f:
+        secret_key = base64.urlsafe_b64encode(os.urandom(length))
         f.write(secret_key)
-        print 'generated secret key: {}'.format(SECRET_FILE)
+        _log('Сгенерирован секретный ключ: {}'.format(SECRET_FILE))
 
 
 def create_user_config_file():
-    print
-    if confirm('Do you want to create your new development configuration?'):
-        username = prompt('Enter your username',
+    if confirm('Создать новую конфигурацию проекта?'):
+        username = prompt('Имя пользователя',
                           default=getpass.getuser(),
                           validate=r'^.*$')
-        with lcd(PROJECT_NAME):
-            local_template_path = 'conf/local_settings.template'
-            result_filename = 'local_%s.py' % username
-            result_file_path = os.path.join(PROJECT_PATH, 'settings',
-                                            result_filename)
 
-            # create local_<user>.py
-            with open(result_file_path, 'w') as dest, open(local_template_path, 'r') as src:
-                dest.write(src.read())
-                print 'created {}'.format(result_file_path)
+        src_settings = _rel('conf/local_settings.template')
+        dst_settings_path = os.path.join(
+            PROJECT_NAME,
+            'settings',
+            'local_{}.py'.format(username)
+        )
+        dst_settings = _rel(dst_settings_path)
 
-            # create config.ini
-            with open('conf/config.ini', 'w') as dest, open('conf/config.ini.template', 'r') as src:
-                dest.write(src.read())
-                print 'created {}'.format(USER_CONFIG_FILE)
+        # Создать local_<user>.py
+        _render(src_settings, dst_settings)
+        _log('Создан файл {}'.format(dst_settings_path))
 
-            # update manage.py
-            manage_py_template = jinja2.Template(open('conf/manage.py.template').read())
-            output = manage_py_template.render(config_name=result_filename[:-3])
-            with open('manage.py', 'w') as f:
-                f.write(output)
-                print 'updated manage.py'
+        # Создать config.ini
+        _render(_rel('conf/config.ini.template'), _rel('conf/config.ini'))
+        _log('Создан файл {}'.format(USER_CONFIG_FILE))
 
-            print ('now you should modify your database settings in {}'
-                   .format(result_file_path))
+        # Обновить manage.py для использования новых настроек
+        config_path = os.path.splitext(dst_settings_path.replace('/', '.'))[0]
+        _render(_rel('conf/manage.py.template'), _rel('manage.py'),
+                config_path=config_path)
+        _log('Обновлён manage.py')
+
+        _log('Осталось указать конфигурацию БД в {}'
+             .format(USER_CONFIG_FILE))
 
 
 def bootstrap():
-    """Installs everything."""
+    """Разворачивает проект в виртуальном окружении."""
     make_virtualenv()
     install_requirements()
     generate_secret()
