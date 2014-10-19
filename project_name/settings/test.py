62825963f663469b2046f859bc99742b55e76262
Stanislav Lobanov <n10101010@gmail.com>
2014-10-19 19:28:41 +0400

    Added test settings

---
+++ project_name/settings/test.py
@@ -0,0 +1,43 @@
+try:
+    from ConfigParser import ConfigParser
+except ImportError:  # python3
+    from configparser import ConfigParser
+
+from {{ project_name }}.settings.base import *
+
+cfg = ConfigParser()
+cfg.readfp(open(os.path.normpath(os.path.join(BASE_DIR, '../conf/config.ini'))))
+
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+SECRET_KEY = 'test_key'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': cfg.get('db', 'db'),
+        'USER': cfg.get('db', 'user'),
+        'PASSWORD': cfg.get('db', 'password'),
+        'HOST': cfg.get('db', 'host'),
+        'PORT': cfg.get('db', 'port'),
+        'CONN_MAX_AGE': 0,
+    }
+}
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.DummyCache',
+    }
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+INTERNAL_IPS = ['127.0.0.1']
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+    'django.template.loaders.eggs.Loader',
+)
