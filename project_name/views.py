1a9fb49c5d2c1babcf339ba8d665d6804670767f
Stanislav Lobanov <n10101010@gmail.com>
2014-12-01 17:52:20 +0300

    Added robots.txt handler

--- project_name/views.py
+++ project_name/views.py
@@ -5,3 +5,8 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html', {})
+
+
+def robots_txt(request):
+    return render(
+        request, 'robots.txt', {}, content_type='text/plain')
