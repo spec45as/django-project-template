107b9a2c7d6315063b2329f38d700cb470bc999a
Stanislav Lobanov <n10101010@gmail.com>
2014-12-01 18:12:10 +0300

    Bugfix with absolute import

--- project_name/celery.py
+++ project_name/celery.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-from __future__ import print_function, unicode_literals, division
+from __future__ import absolute_import
 
 from celery import Celery
 from django.conf import settings
