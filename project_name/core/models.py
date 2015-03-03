f16f57daeaac12982b477aa472d0a650580f0b0b
Stanislav Lobanov <n10101010@gmail.com>
2015-03-03 11:40:23 +0300

    Added DateTimeMixin to core.models

--- project_name/core/models.py
+++ project_name/core/models.py
@@ -4,3 +4,12 @@ from __future__ import print_function, unicode_literals, division
 
 from django.db import models
 
+
+class DateTimeMixin(models.Model):
+    date_created = models.DateTimeField(
+        u'Дата создания', auto_now_add=True)
+    date_updated = models.DateTimeField(
+        u'Дата обновления', auto_now=True)
+
+    class Meta:
+        abstract = True
