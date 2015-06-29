f898b601ab9f4a5c288f1c401017fefef3283294
Stanislav Lobanov <jungo.rnti@gmail.com>
2015-06-29 11:56:06 +0300

    New deployment layout

---
+++ deploy/utils.py
@@ -0,0 +1,53 @@
+import sys
+import subprocess
+
+
+PYTHON_VERSION = sys.version_info.major
+
+
+def local(cmd, shell=None):
+    p = subprocess.Popen(
+        cmd,
+        stdout=None,
+        stderr=subprocess.STDOUT,
+        shell=True,
+        executable=shell,
+    )
+    out, err = p.communicate()
+
+
+def prompt(text, default=''):
+    if default == '':
+        prompt_text = text
+    else:
+        prompt_text = u'{} [{}]'.format(text.strip(), default)
+
+    value = None
+    while value is None:
+        if PYTHON_VERSION == 2:
+            value = raw_input(prompt_text) or default
+        else:
+            value = input(prompt_text) or default
+
+    return value
+
+
+def confirm(question, default=True):
+    if default:
+        choices = "Y/n"
+    else:
+        choices = "y/N"
+
+    while True:
+        response = prompt("{} [{}] ".format(question, choices))
+
+        if not response:
+            return default
+
+        if response.lower() in ['y', 'yes']:
+            return True
+
+        if response.lower() in ['n', 'no']:
+            return False
+
+        print("Выберите вариант: y(es) или (n)o")
