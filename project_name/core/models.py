2c3495e5015cba17ecf608387b33bafb7c890725
Stanislav Lobanov <n10101010@gmail.com>
2015-07-25 19:11:56 +0300

    Removed code compatibility with python 2

--- project_name/core/models.py
+++ project_name/core/models.py
@@ -1,15 +1,13 @@
 # coding: utf-8
 
-from __future__ import print_function, unicode_literals, division
-
 from django.db import models
 
 
 class DateTimeMixin(models.Model):
     date_created = models.DateTimeField(
-        u'Дата создания', auto_now_add=True)
+        'Дата создания', auto_now_add=True)
     date_updated = models.DateTimeField(
-        u'Дата обновления', auto_now=True)
+        'Дата обновления', auto_now=True)
 
     class Meta:
         abstract = True
--- project_name/project_name/admin.py
+++ project_name/project_name/admin.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 from django.contrib import admin
 from django.contrib.flatpages.models import FlatPage
 from django.contrib.flatpages.admin import FlatPageAdmin as FlatPageAdminOld
