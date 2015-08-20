d9d057f85b5d42419f556912a329bcc20b4e0424
Stanislav Lobanov <jungo.rnti@gmail.com>
2015-08-20 15:17:58 +0300

    Added wagtail urls imports

--- project_name/project_name/urls.py
+++ project_name/project_name/urls.py
@@ -5,6 +5,11 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 
+from wagtail.wagtailcore import urls as wagtail_urls
+from wagtail.wagtailadmin import urls as wagtailadmin_urls
+from wagtail.wagtaildocs import urls as wagtaildocs_urls
+from wagtail.wagtailsearch import urls as wagtailsearch_urls
+
 from {{ project_name }} import views
 
 
