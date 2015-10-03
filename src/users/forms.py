8d2bb19f53d0a15a73abf4d990b4b3bfeaa17199
Stanislav Lobanov <n10101010@gmail.com>
2015-10-03 09:01:32 +0300

    Added user form templates

--- src/users/forms.py
+++ src/users/forms.py
@@ -1,5 +1,14 @@
 # coding: utf-8
 
 from django import forms
+from django.contrib.auth import forms as auth_forms
 
 from users import models as m
+
+
+class UserChangeForm(auth_forms.UserChangeForm):
+    pass
+
+
+class UserCreationForm(auth_forms.UserCreationForm):
+    pass
