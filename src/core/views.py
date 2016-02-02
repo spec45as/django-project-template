9997838dbd99d5dcc4e60badefedc7cc1dd70b7d
Stanislav Lobanov <jungo.rnti@gmail.com>
2016-02-02 17:13:52 +0300

    Custom error views

--- src/core/views.py
+++ src/core/views.py
@@ -1,3 +1,33 @@
 # coding: utf-8
 
 from django.shortcuts import render
+from django.conf import settings
+from django.views import csrf
+from django.views import defaults
+
+
+def csrf_failure(request, reason="", force_display=False):
+    if not settings.DEBUG or force_display:
+        return render(request, 'errors/403_csrf.html', {})
+
+    return csrf.csrf_failure(request, reason=reason)
+
+
+def page_not_found(*args, **kwargs):
+    kwargs['template_name'] = 'errors/404.html'
+    return defaults.page_not_found(*args, **kwargs)
+
+
+def server_error(*args, **kwargs):
+    kwargs['template_name'] = 'errors/500.html'
+    return defaults.server_error(*args, **kwargs)
+
+
+def bad_request(*args, **kwargs):
+    kwargs['template_name'] = 'errors/400.html'
+    return defaults.bad_request(*args, **kwargs)
+
+
+def permission_denied(*args, **kwargs):
+    kwargs['template_name'] = 'errors/403.html'
+    return defaults.permission_denied(*args, **kwargs)
--- src/project_name/urls.py
+++ src/project_name/urls.py
@@ -8,6 +8,12 @@ from django.contrib import admin
 from {{ project_name }} import views
 
 
+handler400 = 'core.views.bad_request'
+handler403 = 'core.views.permission_denied'
+handler404 = 'core.views.page_not_found'
+handler500 = 'core.views.server_error'
+
+
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^robots.txt$', views.robots_txt, name='robots_txt'),
@@ -24,6 +30,14 @@ if settings.DEBUG:
         static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) +
         static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     )
+
+    urlpatterns += [
+        url(r'^400/$', 'core.views.bad_request', {'exception': 'test'}),
+        url(r'^403/$', 'core.views.permission_denied', {'exception': 'test'}),
+        url(r'^403_csrf/$', 'core.views.csrf_failure', {'force_display': True}),
+        url(r'^404/$', 'core.views.page_not_found', {'exception': 'test'}),
+        url(r'^500/$', 'core.views.server_error'),
+    ]
 
     import debug_toolbar
     urlpatterns += [
