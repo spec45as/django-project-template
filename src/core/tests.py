b2ab6fc38f85526497e14c30b03ced5c8c459de3
Stanislav Lobanov <n10101010@gmail.com>
2016-02-03 11:45:33 +0300

    Added upload_to helper function

--- src/core/tests.py
+++ src/core/tests.py
@@ -1,3 +1,25 @@
 # coding: utf-8
 
+from hashlib import md5
+
 from django.test import TestCase
+
+from . import utils as u
+
+
+class UploadToTestCase(TestCase):
+
+    def setUp(self):
+        self.filename = 'image.jpg'
+        self.hash = md5(self.filename)
+        self.upload_path = '/'.join(
+            [self.hash[:2], self.hash[2:4], self.hash + '.jpg'])
+
+    def test_upload_to_directory(self):
+        uploader = u.upload_to('dir')
+        expected_dir = 'dir/' + self.upload_path
+        assert uploader(instance=None, filename=self.filename) == expected_dir
+
+    def test_upload_to_empty_directory(self):
+        uploader = u.upload_to()
+        assert uploader(instance=None, filename=self.filename) == self.upload_path
